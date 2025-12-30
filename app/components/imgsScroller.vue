19e38c837a8562b857c7fb7901c98f4f92e6c01c
Amen <amenaf244@gmail.com>
2025-12-30 10:59:09 +0300

    1.7

--- app/components/imgsScroller.vue
+++ app/components/imgsScroller.vue
@@ -7,10 +7,11 @@
   <div class="relative">
     <img src="/Images/Header.jpg">
     <div class="absolute top-[30%] left-[4%] w-[30%]">
-      <h1 class="text-2xl font-bold inline">SALE</h1>
+      <h1 class="font-bold inline
+      xl:text-2xl">SALE</h1>
       <br />
-      <span class="font-bold text-[10px]
-      xl:text-sm">
+      <span class="font-bold text-[10px] leading-1.25
+      xl:text-sm xl:leading-1.5 mb-4">
         ACCESS A LIMITED SELECTION<br /> WITH UP TO 70% OFF
       </span>
       <br />
